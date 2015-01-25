c764935ba11b604501b914d6362553423301cd68
Archer_小A <qddegtya@163.com>
2015-01-25 21:02:38 +0800

    150%

--- assets/js/archer.js
+++ assets/js/archer.js
@@ -61,11 +61,11 @@
         //收菜单栏
         $("#close_nav").click(function(){
             $("#side_nav").css({
-                "-webkit-transform": "translate3d(-100%,0,0)",
-                "-moz-transform": "translate3d(-100%,0,0)",
-                "-ms-transform": "translate3d(-100%,0,0)",
-                "-o-transform": "translate3d(-100%,0,0)",
-                "transform": "translate3d(-100%,0,0)"
+                "-webkit-transform": "translate3d(-150%,0,0)",
+                "-moz-transform": "translate3d(-150%,0,0)",
+                "-ms-transform": "translate3d(-150%,0,0)",
+                "-o-transform": "translate3d(-150%,0,0)",
+                "transform": "translate3d(-150%,0,0)"
             });
         });
 
