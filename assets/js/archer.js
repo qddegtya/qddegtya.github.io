3121338664401fec0b26a18c9fcfb1cf555c8b64
Archer <qddegtya@gmail.com>
2023-01-15 22:07:01 +0800

    feat: add back to top

--- assets/js/archer.js
+++ assets/js/archer.js
@@ -2,6 +2,19 @@
  * Created by archer on 14/10/18.
  */
 
-(function(window, document) {
+(function (window, document, $) {
+  var b2t = $("#b2t");
 
-})(window, document);
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      b2t.addClass("show");
+    } else {
+      b2t.removeClass("show");
+    }
+  });
+
+  b2t.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
+})(window, document, jQuery);
